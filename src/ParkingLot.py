36438894fb732186a7a3e772d1d61ed91b49d15a
sagar <sagar@dunzo.in>
2018-02-07 01:47:50 +0530

    Public interface defined for ParkingLot class

---
+++ src/ParkingLot.py
@@ -0,0 +1,24 @@
+class ParkingLot(object):
+	def __init__(self):
+		pass
+
+	def create_parking_lot(self, lot_space):
+		pass
+
+	def park(self, car_reg_no, car_color):
+		pass
+
+	def leave(self, slot_no):
+		pass
+
+	def status(self):
+		pass
+
+	def registration_numbers_for_cars_with_colour(self, color):
+		pass
+
+	def slot_numbers_for_cars_with_colour(self, color):
+		pass
+
+	def slot_number_for_registration_number(self, reg_no):
+		pass
