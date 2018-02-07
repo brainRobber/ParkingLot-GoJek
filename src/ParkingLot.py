e1d8f14b800bd1c31b042922c797ce788c1d1298
sagar <sagar@dunzo.in>
2018-02-07 13:50:55 +0530

    Parking Lot class constructor written

--- src/ParkingLot.py
+++ src/ParkingLot.py
@@ -1,9 +1,36 @@
 class ParkingLot(object):
+	class Car(object):
+		def __init__(self, reg_no, color):
+			self.reg_no = reg_no
+			self.color = color
+
+		def __str__(self):
+			return "%s %s" % (self.reg_no, self.color)
+
+	map_slot_to_cars = None
+	map_regno_to_slot = None
+	map_color_to_list_of_regno = None
+
 	def __init__(self):
-		pass
+		self.max_size = -1
+		self.slots = None
 
 	def create_parking_lot(self, lot_space):
-		pass
+		try:
+			self.max_size = int(lot_space)
+		except ValueError:
+			print("Invalid Lot Count")
+			return
+
+		try:
+			self.slots = [None for i in range(0, (self.max_size+1))]
+			self.map_slot_to_cars = {}
+			self.map_regno_to_slot = {}
+			self.map_color_to_list_of_regno = {}
+
+			print("created parking lot with %s spaces" % lot_space)
+		except Exception as e:
+			print(e)
 
 	def park(self, car_reg_no, car_color):
 		pass
